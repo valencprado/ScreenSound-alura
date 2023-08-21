15e88a1243ef19584168f8aaa860dc4201cda972
valentina <valenpschool@gmail.com>
2023-08-21 16:32:47 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound/Program.cs
@@ -0,0 +1,36 @@
+﻿// Screen Sound
+string mensagem = "Boas vindas ao Screen Sound!\n\n";
+
+void ExibirMensagemDeBoasVindas()
+{
+    Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+    Console.WriteLine(mensagem);
+
+}
+
+void ExibirMenu()
+{
+    Console.WriteLine("Digite 1 para registrar banda");
+    Console.WriteLine("Digite 2 para listar bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir média de avaliação de uma banda");
+    Console.WriteLine("Digite -1 para sair.");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoNumber = int.Parse(opcaoEscolhida);
+
+    if(opcaoNumber == 1)
+    {
+        Console.WriteLine("Você escolheu: registrar nova banda.");
+    }
+   
+}
+ExibirMensagemDeBoasVindas();
+ExibirMenu();
